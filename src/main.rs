68513a356a9ce3abae4f4eca5d09a555a8c20e7a
Farseen <zah4n5dz@anonaddy.me>
2021-04-29 10:38:10 +0530

    Removing rocket and comments about rocket, because now it seems the bug occurs even without rocket

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,11 @@
 #![feature(backtrace)]
 
-#[macro_use]
-extern crate rocket;
-
 // 1. If I remove SQLx import, backtrace is generated correctly
 use sqlx;
 
 // 2. Backtrace is generated correctly in a non async main() function
 
-// 3. Backtrace is generated correctly in #[tokio::main] async main() function, if rocket is
-//    removed from Cargo.toml
-
-#[rocket::main]
+#[tokio::main]
 async fn main() -> Result<(),()> {
     println!("{}", std::backtrace::Backtrace::force_capture());
     Ok(())
